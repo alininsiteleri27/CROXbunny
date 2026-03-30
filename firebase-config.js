44b8308af351ad8ed4055c0f4019a99cae79416a
reiszali <alininsiteleri27@gmail.com>
2026-03-30 20:43:00 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,13 +4,16 @@
 // ──────────────────────────────────────────────────────────
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDuKLuoePZ6mNsKhQBGXumxMwF0UKTQvc8",
+  authDomain: "oyun-75056.firebaseapp.com",
+  databaseURL: "https://oyun-75056-default-rtdb.firebaseio.com",
+  projectId: "oyun-75056",
+  storageBucket: "oyun-75056.firebasestorage.app",
+  messagingSenderId: "980660244755",
+  appId: "1:980660244755:web:47889c4b6637ab05cdcae6",
+  measurementId: "G-J9RKPSVT8B"
 };
+
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
